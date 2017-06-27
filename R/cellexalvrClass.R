46cff9346f7012bfecc91e07b422dd6b05a049f9
shambam <shamit.soneji@med.lu.se>
2017-06-27 12:59:20 +0200

    initial upload

---
+++ R/cellexalvrClass.R
@@ -0,0 +1,11 @@
+#'Class defintion of cellexalvr
+#' @exportClass cellexalvr
+setClass("cellexalvr",slots=c(
+    data="matrix",
+    meta.cell="matrix",
+    meta.gene="matrix",
+    mds="list",
+    index="matrix"
+    )   
+
+)
