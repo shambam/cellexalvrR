c4eeec7b0e80a25a7a3bd9d87f84ac5bda2f4b92
shambam <shamit.soneji@med.lu.se>
2017-07-04 15:24:29 +0200

    hd

--- R/cellexalvrExportFunctions.R
+++ R/cellexalvrExportFunctions.R
@@ -25,11 +25,11 @@ export2cellexalvr <- function(cellexalObj,path){
     #RSQLite::dbWriteTable(con, "data",mdc)
     #RSQLite::dbDisconnect(con)
 
-    h5createFile(paste(path,"expression.h5",sep=""))
-    h5write(cellexalObj@data,paste(path,"expression.h5",sep=""),"expression")
-    h5write(colnames(cellexalObj@data),paste(path,"expression.h5",sep="")),"cells")
-    h5write(rownames(cellexalObj@data),paste(path,"expression.h5",sep="")),"genes")
-    H5close()
+    #h5createFile(paste(path,"expression.h5",sep=""))
+    #h5write(cellexalObj@data,paste(path,"expression.h5",sep=""),"expression")
+    #h5write(colnames(cellexalObj@data),paste(path,"expression.h5",sep="")),"cells")
+    #h5write(rownames(cellexalObj@data),paste(path,"expression.h5",sep="")),"genes")
+    #H5close()
 }
 
 
