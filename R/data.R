9f77d3a3cea4336cb8dfbcdee88c2ffb5d935d5d
shambam <shamit.soneji@med.lu.se>
2017-07-11 12:09:37 +0200

    data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' A list of mouse transcription factors.
+#'
+#' A list of mouse transcription factors. Used when generating TF networks
+#'
+#' @format A vector 1357 in length:
+#' \describe{
+#'   \item{tf}{transcription factor}
+#'   ...
+#' }
+#' @source \url{http://bioinfo.life.hust.edu.cn/AnimalTFDB/}
+"mouse.tfs"
