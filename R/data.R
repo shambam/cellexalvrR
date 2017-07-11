bfb65e5dc7f52bc5368346401d05e40633769dbe
shambam <shamit.soneji@med.lu.se>
2017-07-11 13:16:20 +0200

    human data

--- R/data.R
+++ R/data.R
@@ -9,3 +9,15 @@
 #' }
 #' @source \url{http://bioinfo.life.hust.edu.cn/AnimalTFDB/}
 "mouse.tfs"
+
+#' A list of human transcription factors.
+#'
+#' A list of human transcription factors. Used when generating TF networks
+#'
+#' @format A vector 1357 in length:
+#' \describe{
+#'   \item{tf}{transcription factor}
+#'   ...
+#' }
+#' @source \url{http://bioinfo.life.hust.edu.cn/AnimalTFDB/}
+"human.tfs"
