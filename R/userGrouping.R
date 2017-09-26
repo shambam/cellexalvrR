dcee6552f6c7daad36143f1372e794888ab1cfa3
Stefan Lang <Stefan.Lang@med.lu.se>
2017-09-26 11:25:22 +0200

    userGrouping now checks if it only got a group name instead of a file.

--- R/userGrouping.R
+++ R/userGrouping.R
@@ -8,46 +8,53 @@
 userGrouping  <- function (cellexalObj, cellidfile) {
 	
 	cellexalObj <- renew(cellexalObj)
-	cellid <- read.delim(cellidfile,header=F)
+	if ( file.exists(cellidfile)){
+		cellid <- read.delim(cellidfile,header=F)
 	
-	grp.vec <- as.vector(cellid[,2])
-	grps <- as.vector(unique(cellid[,2]))
+		grp.vec <- as.vector(cellid[,2])
+		grps <- as.vector(unique(cellid[,2]))
 	
-	req.graph <- unique(as.vector(cellid[,3]))
+		req.graph <- unique(as.vector(cellid[,3]))
 	
-	id= (ncol(cellexalObj@userGroups) /2) + 1
-	gname = paste( "User.group", id, sep="." )
+		id= (ncol(cellexalObj@userGroups) /2) + 1
+		gname = paste( "User.group", id, sep="." )
 	
-	n = vector( 'numeric', ncol(cellexalObj@data))
-	n[match(cellid[,1], colnames(cellexalObj@data)) ] = cellid[,2]
-	n[which(n==0)] = NA
-	order = n
-	order[match(cellid[,1], colnames(cellexalObj@data))] = 1:nrow(cellid)
+		n = vector( 'numeric', ncol(cellexalObj@data))
+		n[match(cellid[,1], colnames(cellexalObj@data)) ] = cellid[,2]
+		n[which(n==0)] = NA
+		order = n
+		order[match(cellid[,1], colnames(cellexalObj@data))] = 1:nrow(cellid)
 	
 	#I need to record the order in the grouping, too!
-	if ( id == 1 ) {
-		cellexalObj@userGroups = data.frame( a = n, b = order )
+		if ( id == 1 ) {
+			cellexalObj@userGroups = data.frame( a = n, b = order )
 		
-	}else {
-		## did we already have this exact grouping?
-		t <- apply( cellexalObj@userGroups, 2, function ( x ) { ok = all.equal(x,n); if ( ok == T ) {TRUE } else { FALSE } } )
-		d <- apply( cellexalObj@userGroups, 2, function ( x ) { ok = all.equal(x,order); if ( ok == T ) {TRUE } else { FALSE } } )
-		ok <- which( t == T )
-		if ( length(ok) == 1 & length( which(d == T)) == 1) {
-			gname = names(ok)
-			id = ceiling(as.vector(ok) / 2)
 		}else {
-			cellexalObj@userGroups = cbind(cellexalObj@userGroups, n, order)
-		}
+			## did we already have this exact grouping?
+			t <- apply( cellexalObj@userGroups, 2, function ( x ) { ok = all.equal(x,n); if ( ok == T ) {TRUE } else { FALSE } } )
+			d <- apply( cellexalObj@userGroups, 2, function ( x ) { ok = all.equal(x,order); if ( ok == T ) {TRUE } else { FALSE } } )
+			ok <- which( t == T )
+			if ( length(ok) == 1 & length( which(d == T)) == 1) {
+				gname = names(ok)
+				id = ceiling(as.vector(ok) / 2)
+			}else {
+				cellexalObj@userGroups = cbind(cellexalObj@userGroups, n, order)
+			}
 		
+		}
+		colnames(cellexalObj@userGroups)[2*id-1] = gname
+		colnames(cellexalObj@userGroups)[2*id] = paste( gname, 'order' )
+		
+		cellexalObj@groupSelectedFrom[[gname]] = req.graph
+		cellexalObj@colors[[gname]] = unique(grps)	
 	}
-	colnames(cellexalObj@userGroups)[2*id-1] = gname
-	colnames(cellexalObj@userGroups)[2*id] = paste( gname, 'order' )
+	else if ( is.na(match(cellidfile, colnames(cellexalObj@userGroups))) ==F ) {
+		gname = cellidfile
+	}else {
+		stop( paste("Sorry I can not process the request for grouping",cellidfile,"\nIt is neither a file nor a known user grouping name\n" ) )
+	}
 	
 	cellexalObj@usedObj$lastGroup = gname
-	
-	cellexalObj@groupSelectedFrom[[gname]] = req.graph
-	cellexalObj@colors[[gname]] = unique(grps)
 	
 	cellexalObj
 }
