d913e4094b2c19564bacb3d50083bdb51e039158
stela2502 <Stefan.Lang@med.lu.se>
2017-09-28 09:46:06 +0200

    Added an install.R for makefile based install process.

--- install.R
+++ install.R
@@ -1,7 +1,14 @@
+try ({
+			update.packages(ask = FALSE, repos='https://ftp.acc.umu.se/mirror/CRAN/')
+ } )
+
+source("https://bioconductor.org/biocLite.R")
+#useDevel(devel=TRUE)
+biocLite(ask=FALSE)
+
 if (!library("devtools", quietly = TRUE,logical.return=TRUE )) {
 	install.packages(c('devtools'),  repos='https://ftp.acc.umu.se/mirror/CRAN/')
 	library(devtools)
 }
-source("https://bioconductor.org/biocLite.R")
-biocLite()
+
 install()
