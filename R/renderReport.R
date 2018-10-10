7ffdf92219e6d1151cfc005461b9db4eb0b0029d
Stefan Lang <Stefan.Lang@med.lu.se>
2018-10-10 07:52:42 +0200

    replace kitrn.R script with direct function calls

--- R/renderReport.R
+++ R/renderReport.R
@@ -34,46 +34,52 @@ setMethod('renderReport', signature = c ('cellexalvrR'),
         ), fileConn )
     close(fileConn)
 	
-	#this part is so buggy I need to export it into a new thread
-	## first a short script
-	cmd = paste(
-			Sys.which('Rscript') , 
-			file.path(sessionPath, 'knit.R' ), 
-			file.path( cellexalObj@outpath , 'cellexalObj.RData') 
-	)
+	oldwd = getwd()
+	setwd( cellexalObj@usedObj$sessionPath )
+	files = as.character(unlist(lapply( cellexalObj@usedObj$sessionRmdFiles, basename)))
+	bookdown::render_book( input=files, output_format='bookdown::gitbook', clean_envir = FALSE )
+	setwd( oldwd )
 	
-	fileConn<-file(file.path(sessionPath, 'knit.R' ) )
-		
-	writeLines(c(
-					"args <- commandArgs(trailingOnly = TRUE)",
-					"library(cellexalvrR)",
-					"#this should now have this useless comment",
-					"cellexalObj = loadObject( args[1] )" ,
-					"setwd(cellexalObj@usedObj$sessionPath)",
-					"message ( getwd())",
-					"files = as.character(unlist(lapply( cellexalObj@usedObj$sessionRmdFiles, basename)))",
-					"message( paste( files ) )",
-					"bookdown::render_book( input=files, output_format='bookdown::gitbook', clean_envir = FALSE )",
-					paste( sep=" ", "#", cmd )
-			), fileConn )
-	close(fileConn)
 	
-	#print ( cmd )
-	system( 
-			paste(
-					Sys.which('Rscript') , 
-					file.path(sessionPath, 'knit.R' ), 
-					file.path( cellexalObj@outpath , 'cellexalObj.RData') 
-			)
-			)
-
-	for ( i in 1:6 ){
-		if (file.exists( file.path(sessionPath, paste(cellexalObj@usedObj$sessionName, sep='.', 'html') )) ){
-			last
-		}
-		Sys.sleep(10)
-	}
-	
+#	#this part is so buggy I need to export it into a new thread
+#	## first a short script
+#	cmd = paste(
+#			Sys.which('Rscript') , 
+#			file.path(sessionPath, 'knit.R' ), 
+#			file.path( cellexalObj@outpath , 'cellexalObj.RData') 
+#	)
+#	fileConn<-file(file.path(sessionPath, 'knit.R' ) )
+#		
+#	writeLines(c(
+#					"args <- commandArgs(trailingOnly = TRUE)",
+#					"library(cellexalvrR)",
+#					"#this should now have this useless comment",
+#					"cellexalObj = loadObject( args[1] )" ,
+#					"setwd(cellexalObj@usedObj$sessionPath)",
+#					"message ( getwd())",
+#					"files = as.character(unlist(lapply( cellexalObj@usedObj$sessionRmdFiles, basename)))",
+#					"message( paste( files ) )",
+#					"bookdown::render_book( input=files, output_format='bookdown::gitbook', clean_envir = FALSE )",
+#					paste( sep=" ", "#", cmd )
+#			), fileConn )
+#	close(fileConn)
+#	
+#	#print ( cmd )
+#	system( 
+#			paste(
+#					Sys.which('Rscript') , 
+#					file.path(sessionPath, 'knit.R' ), 
+#					file.path( cellexalObj@outpath , 'cellexalObj.RData') 
+#			)
+#			)
+#
+#	for ( i in 1:6 ){
+#		if (file.exists( file.path(sessionPath, paste(cellexalObj@usedObj$sessionName, sep='.', 'html') )) ){
+#			last
+#		}
+#		Sys.sleep(10)
+#	}
+#	
 	if ( file.exists( file.path(sessionPath, paste(cellexalObj@usedObj$sessionName, sep='.', 'html')) )){
 		cellexalObj@usedObj$sessionPath = cellexalObj@usedObj$sessionRmdFiles = cellexalObj@usedObj$sessionName = NULL
 		lockedSave(cellexalObj)
