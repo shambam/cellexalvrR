5eb840026e758a6816db3d32b5aeadf15778b6cb
shambam <shamit.soneji@med.lu.se>
2019-04-17 09:53:25 +0200

    added a function to make a rhop network

--- R/NetworkFunctions.R
+++ R/NetworkFunctions.R
@@ -33,18 +33,18 @@ setMethod('cormat2df', signature = c ('cellexalvrR'),definition = function (cors
 #' @param cellidfile file containing cell IDs
 #' @param outpath the outpath
 #' @param cutoff.ggm The cutoff for the correlation (default = 0.8)
-#' @param top.n.inter get only the n top interations default=125
+#' @param top.n.inter get only the n top interations default=130
 #' @title description of function make.cellexalvr.network
 #' @keywords network construction
 #' @export make.cellexalvr.network
 if ( ! isGeneric('make.cellexalvr.network') ){setGeneric('make.cellexalvr.network', ## Name
-	function (cellexalObj, cellidfile,outpath, cutoff.ggm=0.8,top.n.inter=125) {
+	function (cellexalObj, cellidfile,methods=c("rho.p","pcor"),outpath, cutoff.ggm=0.8,top.n.inter=130) {
 		standardGeneric('make.cellexalvr.network')
 	}
 ) }
 
 setMethod('make.cellexalvr.network', signature = c ('cellexalvrR'),
-	definition = function (cellexalObj, cellidfile,outpath, cutoff.ggm=0.8, top.n.inter=125) {
+	definition = function (cellexalObj, cellidfile,outpath, cutoff.ggm=0.8, top.n.inter=130) {
 
 		if ( !file.exists(outpath)) {
 			dir.create( outpath ,  recursive = TRUE)
@@ -84,48 +84,84 @@ setMethod('make.cellexalvr.network', signature = c ('cellexalvrR'),
     avg.mds.coods <- NULL
     #layout.tabs <- NULL
 
-    for(i in 1:length(grps)){
+    if(method=="rho.p"){
 
-        print(paste("Making network",i))
+        for(i in 1:length(grps)){
 
-        rq.cells <- as.vector(colnames(dat)[which(info$grouping==grps[i])])
+            print(paste("Making network",i))
 
-        sub.d <- dat[, rq.cells ]
-        print(dim(sub.d))
+            rq.cells <- as.vector(colnames(dat)[which(info$grouping==grps[i])])
 
-        inferred.pcor <- GeneNet::ggm.estimate.pcor(as.matrix(Matrix::t(sub.d)),method="static")
-        test.results <- GeneNet::network.test.edges(inferred.pcor,plot=F)
-        net <- GeneNet::extract.network(test.results, cutoff.ggm = cutoff.ggm )
+            sub.d <- dat[, rq.cells ]
+            print(dim(sub.d))
 
-        if(nrow(net)>top.n.inter){
-            net <- net[1:top.n.inter,]
+            cor.mat <- propr::perb(as.matrix(t(sub.d)))@matrix
+            cor.mat.flt <- cormat2df(cor.mat)
+            cor.mat.ord <-  cor.mat.flt[rev(order(cor.mat.flt[,3])),]
+            
+            hi.prop <- length(which(cor.mat.ord[,3] > 0.2))
+            lo.prop <- length(which(cor.mat.ord[,3] < -0.2))
+
+            hi.num <- round(top.n.inter*(hi.prop/(hi.prop+lo.prop)))
+            lo.num <- top.n.inter-hi.num
+
+            cor.mat.req <- cor.mat.ord[ c(1:hi.num, (nrow(cor.mat.ord)-lo.num):nrow(cor.mat.ord)),]
+
+            net <- cbind(cor.mat.req[,c(3,1,2)],0,0,0)
+            colnames(net) <- c("pcor","node1","node2","pval","qval","prob")
+
+            avg.mds.coods <- rbind(avg.mds.coods, c(apply(cellexalObj@mds[[req.graph]][rq.cells,],2,mean),info$col[i]))
+
+             if(nrow(cor.mat.req)>0){
+
+                net[,2] <- rownames(sub.d)[net[,2]]
+                net[,3] <- rownames(sub.d)[net[,3]]
+
+                key1 <- paste(net[,2],net[,3],sep="")
+                key2 <- paste(net[,3],net[,2],sep="")
+
+                grp.tabs <- rbind(grp.tabs,cbind(net,info$col[i],key1,key2))
+
+            }else{next}
         }
-
-        avg.mds.coods <- rbind(avg.mds.coods, c(apply(cellexalObj@mds[[req.graph]][rq.cells,],2,mean),info$col[i]))
-
-        if(nrow(net)>0){
-
-            net[,2] <- rownames(sub.d)[net[,2]]
-            net[,3] <- rownames(sub.d)[net[,3]]
-
-            key1 <- paste(net[,2],net[,3],sep="")
-            key2 <- paste(net[,3],net[,2],sep="")
-
-            grp.tabs <- rbind(grp.tabs,cbind(net,info$col[i],key1,key2))
-
-            #igrp <- graph_from_data_frame(as.data.frame(net[,2:3]), directed = TRUE)
-
-            #lay <- round(layout_nicely(igrp),6)
-            #rownames(lay) <- names(V(igrp))
-            #lay[,1] <- rescale(lay[,1],to=c(-1,1))
-            #lay[,2] <- rescale(lay[,2],to=c(-1,1))
-            #lay <- cbind(lay,info$col[i])
-
-            #layout.tabs <- rbind(layout.tabs,lay)
-        }else{next}
-
+    
     }
 
+    if(method=="pcor"){
+
+        for(i in 1:length(grps)){
+
+            print(paste("Making network",i))
+
+            rq.cells <- as.vector(colnames(dat)[which(info$grouping==grps[i])])
+
+            sub.d <- dat[, rq.cells ]
+            print(dim(sub.d))
+
+            inferred.pcor <- GeneNet::ggm.estimate.pcor(as.matrix(Matrix::t(sub.d)),method="static")
+            test.results <- GeneNet::network.test.edges(inferred.pcor,plot=F)
+            net <- GeneNet::extract.network(test.results, cutoff.ggm = cutoff.ggm )
+
+            if(nrow(net)>top.n.inter){
+                net <- net[1:top.n.inter,]
+            }
+
+            avg.mds.coods <- rbind(avg.mds.coods, c(apply(cellexalObj@mds[[req.graph]][rq.cells,],2,mean),info$col[i]))
+
+            if(nrow(net)>0){
+
+                net[,2] <- rownames(sub.d)[net[,2]]
+                net[,3] <- rownames(sub.d)[net[,3]]
+
+                key1 <- paste(net[,2],net[,3],sep="")
+                key2 <- paste(net[,3],net[,2],sep="")
+
+                grp.tabs <- rbind(grp.tabs,cbind(net,info$col[i],key1,key2))
+
+            }else{next}
+
+        }
+    }
 	message( paste( "the cellexal object mds names:", paste( collapse= ", ", names(cellexalObj@mds))))
 	
     if(nrow(grp.tabs)==0){
