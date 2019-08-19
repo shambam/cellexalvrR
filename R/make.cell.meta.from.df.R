868ce2dd0532cd1a8ffd9f0f16a07745b16fb0d4
shambam <shamit.soneji@med.lu.se>
2019-08-19 14:38:55 +0200

    Added camelcase function to make meta cell

--- R/make.cell.meta.from.df.R
+++ R/make.cell.meta.from.df.R
@@ -31,3 +31,40 @@ setMethod('make.cell.meta.from.df', signature = c ('data.frame'),
 	colnames(meta4cellexalvr) <- gsub( '\\s+', '.', perl=T, colnames(meta4cellexalvr))
     meta4cellexalvr
 } )
+
+
+#' The cell meta for cellexalvrR is a list of columns with 0/1 information for all groupings.
+#' As this is not common format this function will convert a multi named grouping 
+#' like CellType=c('HSC', MPP1', MPP2','HSC',...) into 3 columns that CellexalVR can use.
+#' @name makeCellMetaFromDataframe
+#' @aliases makeCellMetaFromDataframe,data.frame-method
+#' @rdname makeCellMetaFromDataframe-methods
+#' @docType methods
+#' @description  Creates a meta cell matrix from a supplied dataframe from required fields
+#' @param metad A dataframe of per cell metadata
+#' @param rq.fields A vector of name specifiying which columns should me made into metadata
+#' @keywords metadata cell
+#' @title description of function makeCellMetaFromDataframe
+#' @export makeCellMetaFromDataframe
+if ( ! isGeneric('makeCellMetaFromDataframe') ){setGeneric('makeCellMetaFromDataframe', ## Name
+	function (metad,rq.fields) { 
+		standardGeneric('makeCellMetaFromDataframe') 
+	}
+) }
+
+setMethod('makeCellMetaFromDataframe', signature = c ('data.frame'),
+	definition = function (metad,rq.fields) {
+
+    meta4cellexalvr <- NULL
+
+    for(i in 1:length(rq.fields)){
+        tmp.met <- phytools::to.matrix(metad[,rq.fields[i]], unique(metad[,rq.fields[i]]) )
+        colnames(tmp.met) <- paste(rq.fields[i], colnames(tmp.met), sep="@")
+        meta4cellexalvr <- cbind(meta4cellexalvr, tmp.met)
+    }
+	# match any repeat of whitespace and replace it with one '.' each
+	colnames(meta4cellexalvr) <- gsub( '\\s+', '.', perl=T, colnames(meta4cellexalvr))
+    meta4cellexalvr
+} )
+
+
