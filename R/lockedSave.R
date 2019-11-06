bc99781aa1957d58209311cf3b96148c69e03d82
Stefan Lang <Stefan.Lang@med.lu.se>
2019-11-06 12:59:30 +0100

    merged Master back to timeline

--- R/lockedSave.R
+++ R/lockedSave.R
@@ -88,7 +88,7 @@ setMethod('loadObject', signature = c ('character'),
 					stop( paste("Could not obtain access to locked file", fname ))
 				}
 			}else {
-				stop( paste( "file does not exixit", fname) )
+				stop( paste( "file does not exist", fname) )
 			}
 			if ( ! is.null(attributes(cellexalObj@class)$package) ) {
 				if ( attributes(cellexalObj@class)$package == 'cellexalvr' ){
@@ -111,6 +111,44 @@ setMethod('loadObject', signature = c ('character'),
 				rm(new)
 				gc()
 			}
+			## this is the function the VR uses to load an object.
+			## and we need to check if our drc names make sense!
+			drcFiles = list.files( dirname( fname ), full.names = TRUE, pattern="*.mds" )
+			
+			if ( length( drcFiles ) > 0) {
+				fnames = unlist( lapply( drcFiles, basename) )
+				fnames = str_replace_all( fnames, '.mds$', '' )
+				m = match(names(cellexalObj@drc), fnames )
+				if ( length(m) != length(fnames) | length(which(is.na(m))) > 0 ) {
+					message("The drc names between VR and R do not overlap - updating the R object!")
+					cellexalObj@drc = lapply(drcFiles, function(n){ 
+						d = read.delim( n, header=F )
+						if ( d[1,1] == 'CellID' ){
+							colnames(d) = d[1,]
+							d= d[-1,]
+						}else {
+							if ( ncol(d) == 4 ) {
+								colnames(d) = c('CellID','dim1','dim2','dim3')
+							}else if (ncol(d) == 7 ) {
+								colnames(d) = c('CellID','dim1','dim2','dim3','velo1','velo2','velo3' )
+							}else {
+								stop( paste("The file", n,"has the wrong format!"))
+							}
+						}
+						rownames(d) = d[,1]
+						d= d[,-1]
+						m = match(colnames(cellexalObj@data), rownames(d))
+						d=d[m,]
+						rownames(d) = colnames(cellexalObj@data)
+						d
+					} )
+
+					names(cellexalObj@drc) = fnames
+					message(paste("Saving the updated R object to", dirname( fname ) ) )
+					lockedSave(cellexalObj, dirname( fname ) )
+				}
+			}
+			
 			#tmp = new('cellexalvrR')
 			#reload = 0
 			if ( ! file.exists(cellexalObj@outpath )) {
