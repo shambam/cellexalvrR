114e126f1026bb2bc1a4f9a587f288b273752042
Stefan Lang <Stefan.Lang@med.lu.se>
2020-08-05 09:50:32 +0200

    as_cellexalvr Seurat now always gets correct drc objects.

--- helperScripts/PutNewCodeHere.R
+++ helperScripts/PutNewCodeHere.R
@@ -1,8 +1,13 @@
-## each cluster in the grouping file should be processed as separate timeline and information put into a log section
-## the log section should plot the group in 2x2D drc + blue red timeline and probably a heatmap with the cells order by time (blue to red)
-timelineAnalysis <- function( x, grouping, n.genes = 200 ) {
-	
-	x <- userGrouping(x, grouping)
-	
-
+## the whole logic will be quite interesting.
+## Outline:
+## UMAP from all 3 objects with an unified cell lable.
+## expression and chrmosomal areas if possible
+## The total analysis likely needs a merge with the CellexalGenomeVR code.
+process_Seurat_ATAC <- function( rna, atac, merged, meta.cell.groups=NULL, outpath=getwd(), specie ){
+		## the whole logic will be quite interesting.
+		## Outline:
+		## UMAP from all 3 objects with an unified cell lable.
+		## expression and chrmosomal areas if possible
+		## The total analysis likely needs a merge with the CellexalGenomeVR code.
+		 
 }
