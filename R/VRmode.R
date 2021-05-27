67752e19d15d5c99dc7ce1f19fc76d73d196443c
Stefan Lang <Stefan.Lang@med.lu.se>
2021-05-27 14:57:06 +0200

    VR mode fixed.

---
+++ R/VRmode.R
@@ -0,0 +1,29 @@
+#' @name VRmode
+#' @aliases VRmode,character-method
+#' @rdname VRmode-methods
+#' @docType methods
+#' @description Windows specific check if CellexalVR.exe is running
+#' @returns boolean value if the VR application is running. 
+#' @keywords VRmode
+#' @title check if CellexalVR.exe is running (Windows specific)
+#' @export 
+if ( ! isGeneric('VRmode') ){setGeneric('VRmode', ## Name
+			function ( file, sleepT=1, debug=FALSE, masterPID = NULL, asFunction =FALSE ) { 
+				standardGeneric('VRmode') 
+			}
+	) }
+
+setMethod('VRmode', signature = c (),
+		definition =  function(file, sleepT=1, debug=FALSE, masterPID = NULL, asFunction =FALSE ){
+	tryCatch ( {
+		task <- shell('tasklist /fi "imagename eq CellexalVR.exe" /nh /fo csv',
+			intern = TRUE)
+		OK = grepl("CellexalVR.exe", task)
+		}, 
+	warning = function(err){
+		## likely not working! likely tasklist not available and hence 
+		OK = FALSE
+    	}
+	)
+	OK
+})
